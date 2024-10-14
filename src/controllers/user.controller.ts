e91220e8ccaecbb5af69feb89ba55ca24a5f50ca
davinavarro998 <davinavarro@2ndmarket.com.br>
2024-10-14 16:50:12 -0300

    try-catch validation added

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,65 +1,96 @@
-import { NextFunction, Request, Response } from "express";
-import { createService, findAllService, findByIdService, updateService } from "../services/user.service";
+import { Request, Response } from "express";
+import {
+  createService,
+  findAllService,
+  findByIdService,
+  updateService,
+} from "../services/user.service";
 import { IUser } from "../models/User";
 
-import mongoose from "mongoose";
-
 export async function create(req: Request, res: Response) {
-  const { name, email, username, avatar, password, background } = req.body;
-  if (!name || !email || !username || !avatar || !password || !background) {
-    return res
-      .status(400)
-      .json({ error: "Submit all fields for registration" }); //Bad request
-  }
+  try {
+    const { name, email, username, avatar, password, background } = req.body;
+    if (!name || !email || !username || !avatar || !password || !background) {
+      res.status(400).json({ error: "Submit all fields for registration" }); //Bad request
+      return;
+    }
 
-  const user:IUser = await createService(req.body);
+    const user: IUser = await createService(req.body);
 
-  if (!user) {
-    return res.status(400).json({ message: "Error creating User" });
-  }
+    if (!user) {
+      res.status(400).json({ message: "Error creating User" });
+      return;
+    }
 
-  return res
-    .status(201)
-    .json({
+    res.status(201).json({
       message: "Congratulations!",
       user: { name, email, username, avatar, background },
     });
-}
 
-
-export async function findAll(req:Request, res:Response) {
-  const users = await findAllService();
-
-  if(!users){
-    return res.status(400).json({message:"There are no registered users"});
+    return;
+  } catch (error) {
+    res.status(500).json({ message: error });
+    return;
   }
-
-
-  return res.status(200).json(users);
 }
 
+export async function findAll(req: Request, res: Response) {
+  try {
+    const users = await findAllService();
 
-export async function findById(req:Request, res:Response) {
-  const id:string = `${req.params.id}`;
+    if (!users) {
+      res.status(400).json({ message: "There are no registered users" });
+      return;
+    }
 
-  const user = await findByIdService(id);
-
-  return res.status(200).json(user);
-}
-
-
-export async function update(req:Request, res:Response) {
-  const { name, email, username, avatar, password, background } = req.body;
-  if (!name && !email && !username && !avatar && !password && !background) {
-    return res
-      .status(400)
-      .json({ error: "Submit at least one field for registration" }); //Bad request
+    res.status(200).json(users);
+    return;
+  } catch (error) {
+    res.status(500).json({ message: error });
+    return;
   }
+}
 
-  const id:string = req.params.id;
+export async function findById(req: Request, res: Response) {
+  try {
+    const id: string = `${req.params.id}`;
 
-  await updateService(id, name, email, username, avatar, password, background);
+    const user = await findByIdService(id);
 
-  return res.json({message:"User was updated succesfully!"});
+    res.status(200).json(user);
+    return;
+  } catch (error) {
+    res.status(500).json({ message: error });
+    return;
+  }
+}
 
+export async function update(req: Request, res: Response) {
+  try {
+    const { name, email, username, avatar, password, background } = req.body;
+    if (!name && !email && !username && !avatar && !password && !background) {
+      res
+        .status(400)
+        .json({ error: "Submit at least one field for registration" }); //Bad request
+      return;
+    }
+
+    const id: string = req.params.id;
+
+    await updateService(
+      id,
+      name,
+      email,
+      username,
+      avatar,
+      password,
+      background
+    );
+
+    res.json({ message: "User was updated succesfully!" });
+    return;
+  } catch (error) {
+    res.status(500).json({ message: error });
+    return;
+  }
 }
--- src/middlewares/global.middlewares.ts
+++ src/middlewares/global.middlewares.ts
@@ -2,31 +2,38 @@ import { Request, Response, NextFunction } from "express";
 import mongoose from "mongoose";
 import { findByIdService } from "../services/user.service";
 
-const isValidId = (req:Request, res:Response, next:NextFunction) => {
-    const id:string = `${req.params.id}`;
+const isValidId = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const id: string = `${req.params.id}`;
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        res.status(400).json({ message: "Invalid user ID format" });
-        return;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      res.status(400).json({ message: "Invalid user ID format" });
+      return;
     }
 
     next();
-}
+  } catch (error) {
+    res.status(500).json({ message: error });
+    return;
+  }
+};
 
-const isValidUser = async (req:Request, res:Response, next:NextFunction) => {
-    const id:string = `${req.params.id}`;
+const isValidUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const id: string = `${req.params.id}`;
 
     const user = await findByIdService(id);
 
-    if(!user) {
-        res.status(400).json({message:"User not found"});
-        return;
+    if (!user) {
+      res.status(400).json({ message: "User not found" });
+      return;
     }
 
     next();
-}
+  } catch (error) {
+    res.status(500).json({ message: error });
+    return;
+  }
+};
 
-export {
-    isValidId,
-    isValidUser
-}
+export { isValidId, isValidUser };
